3b76f7f553306b758eef56e0d36d4aab6e2f6b45
olga.kinakh <olga.kinakh@ingenious.build>
2023-12-09 13:10:49 +0200

    all

---
+++ src/store/templateExample.ts
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useInfoStore = defineStore('info', {
+    state: () => {
+        return {
+            content: {
+                rus: {},
+                rom: {},
+                en: {}
+            },
+        }
+    },
+})
---
+++ src/views/MainView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <slot/>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        name: 'MainView',
+    })
+</script>
+
+<style scoped lang="scss">
+.container {
+  margin: 0 10%;
+  @include mobile {
+    margin: 0 5%;
+    overflow-x: hidden;
+  }
+}
+</style>
